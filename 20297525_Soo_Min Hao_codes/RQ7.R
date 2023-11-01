c693e12d79509d67f0274957d4030ed2de2e8280
SOOPG <wilem.mhsoo@gmail.com>
2023-11-01 13:24:39 +0800

    Initial commit
    -Added mandatory files such as:
    1. Report File
    2. The Dataset used
    3. The dataset in excel file
    4. The folder containing all the .R source file for each question

---
+++ 20297525_Soo_Min Hao_codes/RQ7.R
@@ -0,0 +1,5 @@
+library(readxl)
+
+Steam_Games_Dataset <- read_excel("C:/Users/wilem/OneDrive/Desktop/Desktop Apps/UNM/Year 3/Fundamentals of InfoViz/CW1/Information-Visualisation-on-Steam-Game-Datasets/Steam Games Dataset.xlsx")
+View(Steam_Games_Dataset)
+
