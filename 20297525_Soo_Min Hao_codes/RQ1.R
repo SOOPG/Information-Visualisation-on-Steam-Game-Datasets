72049c3213e99f1119d7b4840d49c5a771c18587
SOOPG <wilem.mhsoo@gmail.com>
2023-11-02 14:09:07 +0800

    RQ1 Completed
    completion of RQ1 code and its report

--- 20297525_Soo_Min Hao_codes/RQ1.R
+++ 20297525_Soo_Min Hao_codes/RQ1.R
@@ -2,29 +2,36 @@
 pacman::p_load(pacman,dplyr,GGally,ggplot2,ggthemes,ggvis,httr,lubridate,plotly,rio,rmarkdown,shiny,stringr,tidyr)
 library(readxl)
 
-#Read Dataset
-Steam_Games_Dataset <- read_excel("C:/Users/wilem/OneDrive/Desktop/Desktop Apps/UNM/Year 3/Fundamentals of InfoViz/CW1/Information-Visualisation-on-Steam-Game-Datasets/Steam Games Dataset.xlsx")
+# Read the dataset
+file_path <- "C:/Users/wilem/OneDrive/Desktop/Desktop Apps/UNM/Year 3/Fundamentals of InfoViz/CW1/Information-Visualisation-on-Steam-Game-Datasets/Steam Games Dataset.xlsx"
+Steam_Games_Dataset <- read_excel(file_path)
 
-##For Number of Games Released On Steam Platform Each Year
-# Adjust Release Date by removing "UTC" and convert to Date format
+# Data cleaning and transformation
+# Remove "UTC" from Release Date and convert to Date format
 Steam_Games_Dataset$`Release Date` <- as.Date(gsub(" UTC", "", Steam_Games_Dataset$`Release Date`), format="%Y-%m-%d")
-all_years <- data.frame(Year = 2000:2023)
 
-# Plotting with ggplot
-p <- Steam_Games_Dataset %>% 
+# Filter out games released before 2003
+Steam_Games_Dataset <- Steam_Games_Dataset %>%
+  filter(year(`Release Date`) >= 2003)
+
+# Preparing data for visualization
+all_years <- data.frame(Year = 2003:2023)
+game_release_data <- Steam_Games_Dataset %>%
   group_by(Year = year(`Release Date`)) %>%
   summarise(Count = n()) %>%
   right_join(all_years, by = "Year") %>%
-  replace_na(list(Count = 0)) %>%
+  replace_na(list(Count = 0))
+
+# Visualization using ggplot
+p <- game_release_data %>%
   ggplot(aes(x = Year, y = Count)) +
   geom_bar(stat = "identity", fill = "steelblue") +
-  scale_x_continuous(breaks = 2000:2023, minor_breaks = NULL) +
+  scale_x_continuous(breaks = 2003:2023, minor_breaks = NULL) +
   labs(title = "Number of Games Released Each Year on Steam Platform",
-       x = "Year", y = "Number of Games") +
+       x = "Year", y = "Number of Games Released on That Year") +
   theme_minimal()
 
-#ggplot to plotly
+# Convert ggplot to plotly for interactivity
 interactive_plot <- ggplotly(p)
 interactive_plot
 
-##For Average Price Trend of Games Over The Years
